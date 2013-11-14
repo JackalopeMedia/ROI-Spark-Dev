fb11d49e60ef2e5b9b4e431d1ec8b22488aa5108
Mason Lawlor <mason.lawlor@me.com>
2013-11-14 17:35:59 -0600

    adding custom fields pt 2 - error
    isn't saving custom post data when published

--- localdev/wp-content/themes/roispark/functions.php
+++ localdev/wp-content/themes/roispark/functions.php
@@ -18,4 +18,79 @@ function register_my_menus() {
 }
 
 add_action('init', 'register_my_menus');
+
+
+/*******************************/
+/* Add Meta Boxes to the Posts */
+/*******************************/	
+add_action('add_meta_boxes', 'custom_add_meta_box');
+
+function custom_add_meta_box() {
+	add_meta_box(
+		'portfolio_details',		// ID
+		'Portfolio Entry Details',	// Title
+		'custom_display_meta_box',	// Callback
+		'post',						// Targeted Post type
+		'normal'					// Position				
+	);
+}
+
+function custom_display_meta_box($post) {
+	$portfolio_description = get_post_meta($post->ID, 'porfolio_description', true);
+	$portfolio_link = get_post_meta($post->ID, 'porfolio_link', true);
+	$portfolio_quote = get_post_meta($post->ID, 'porfolio_quote', true);
+	$portfolio_quote_author = get_post_meta($post->ID, 'porfolio_quote_author', true);
+	
+	// Security Check
+	wp_nonce_field('portfolio_meta_nonce', 'portfolio_nonce');
+	
+	// Display fields
+	?>
+	
+	<p>
+		<label for="portfolio_description">Project Description:</label>
+		<textarea class="widefat" name="portfolio_description" id="portfolio_description" cols="30" rows="10"><?php echo $portfolio_description; ?></textarea>
+	</p>
+	
+	<p>
+		<label for="portfolio_link">Link:</label><br />
+		<input type="text" name="portfolio_link" id="porfolio_link" value="<?php echo $portfolio_link; ?>" />
+	</p>
+	
+	<p>
+		<label for="porfolio_quote">Quote:</label>
+		<textarea class="widefat" name="portfolio_quote" id="portfolio_quote" cols="30" rows="10"><?php echo $portfolio_quote; ?></textarea>
+	</p>
+	
+	<p>
+		<label for="">Quote Author:</label><br />
+		<input type="text" name="portfolio_quote_author" id="porfolio_quote_author" value="<?php echo $portfolio_quote_author; ?>" />
+	</p>
+	
+	<?php
+}
+
+add_action('save_post', 'custom_save_portfolio_details');
+
+function custom_save_portfolio_details($post_id) {
+	// If we're doing an autosave, return
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+	
+	// If nonce is not present or invalid, return
+	if (!isset($_POST['portfolio_nonce']) || !wp_verify_nonce($_POST['portfolio_nonce'], 'portfolio_meta_nonce')) return;
+	
+	// Save/Update Data
+	if (isset($_POST['portfolio_description']) && $_POST['portfolio_description'] != '') {
+		update_post_meta($post_id, 'portfolio_description', esc_html($_POST['portfolio_description']));
+	}
+	if (isset($_POST['portfolio_link']) && $_POST['portfolio_link'] != '') {
+		update_post_meta($post_id, 'portfolio_link', esc_url($_POST['portfolio_link']));
+	}
+	if (isset($_POST['portfolio_quote']) && $_POST['portfolio_quote'] != '') {
+		update_post_meta($post_id, 'portfolio_quote', esc_html($_POST['portfolio_quote']));
+	}
+	if (isset($_POST['portfolio_quote_author']) && $_POST['portfolio_quote_author'] != '') {
+		update_post_meta($post_id, 'portfolio_quote_author', esc_html($_POST['portfolio_quote_author']));
+	}
+}
 ?>
